dfdccc1ed8019dc2206f23f30a1968e6e8f8566a
bds6575 <brian.sanchez2693@gmail.com>
2024-04-26 00:27:44 -0500

    both index work and posts display and can be deleted

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,3 @@
-
 // Retrieve posts from localStorage
 function getBlogPosts() {
     const posts = JSON.parse(localStorage.getItem('posts')) || [];
@@ -16,11 +15,25 @@ function displayPosts() {
     // Create HTML elements for each post
     posts.forEach((post) => {
         const article = document.createElement('article');
+        article.classList.add('blog-post'); // Add the 'blog-post' class
         article.innerHTML = `
             <h2>${post.title}</h2>
             <p>${post.content}</p>
             <p>Author: ${post.username}</p>
+            <button class="delete-post">Delete</button> <!-- Add the delete button -->
         `;
+
+        // Add event listener to the delete button
+        const deleteButton = article.querySelector('.delete-post');
+        deleteButton.addEventListener('click', () => {
+            // Remove the parent article when the delete button is clicked
+            article.remove();
+
+            // Update localStorage to remove the deleted post
+            const updatedPosts = posts.filter((p) => p.title !== post.title);
+            localStorage.setItem('posts', JSON.stringify(updatedPosts));
+        });
+
         blogPostsSection.appendChild(article);
     });
 }
